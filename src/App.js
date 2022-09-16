9f175914c99b6c397c800e57287271750f081fd6
AmanSingh25 <aman.singh98162@gmail.com>
2022-09-16 18:14:34 -0500

    fetches API data and adds search bar to the app

--- src/App.js
+++ src/App.js
@@ -2,36 +2,20 @@ import React, { useState, useEffect } from "react";
 import MovieList from "./components/MovieList";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox"
+
 
 const App = () => {
-  const [movies, setMovies] = useState([{
-    "Title": "Lego Star Wars: All-Stars",
-    "Year": "2018",
-    "imdbID": "tt9297164",
-    "Type": "series",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BOTEwMzhiNjUtOTA3My00MDljLTgxMmItYzdiMTIwYjRjZTM0XkEyXkFqcGdeQXVyMjA0MzE3ODI@._V1_SX300.jpg"
-},
-{
-    "Title": "The Stars of 'Star Wars': Interviews from the Cast",
-    "Year": "1999",
-    "imdbID": "tt0214158",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BMjI3NzcyNTU1MV5BMl5BanBnXkFtZTcwNzI0MDAyMQ@@._V1_SX300.jpg"
-},
-{
-    "Title": "Star Wars Lost Stars",
-    "Year": "2022–",
-    "imdbID": "tt15109530",
-    "Type": "series",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BZTU1MzljNDAtMThjMi00Y2FmLWJkZTItZmU1ZWRjNjEwNzg0XkEyXkFqcGdeQXVyMTI2MTIzOTU5._V1_SX300.jpg"
-},
-]);
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
 const getMovieRequest = async () => {
-  const url = "http://www.omdbapi.com/?i=tt3896198&apikey=9c5b9a3b"
+  const url = "http://www.omdbapi.com/?s=stars wars&apikey=9c5b9a3b"
   const response = await fetch(url);
   const responseJson = await response.json();
-  console.log(responseJson);
+  console.log(responseJson)
+  setMovies(responseJson.Search);
 
 };
 
@@ -41,9 +25,13 @@ useEffect(() => {
 
   return(
    <div className='container-fluid movie-app'>
-    <div className='row'>
-      <MovieList movies={movies} />
+    <div className="row">
+      <MovieListHeading heading="Movies"/>
+      <SearchBox />
     </div>
+      <div className='row'>
+        <MovieList movies={movies} />
+      </div>
   </div>
   )
 }
---
+++ src/components/SearchBox.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchBox = (props) => {
+    return (
+        <div className="col col-sm-4">
+            <input className="form-control" placeholder="type to search"></input>
+        </div>
+    )
+}
+
+export default SearchBox
